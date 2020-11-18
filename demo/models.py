38283e3d501a3a8cc604c03f45874d4f4d1431ec
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-18 10:59:09 +0000

    Added a TextField field type for a description field

--- demo/models.py
+++ demo/models.py
@@ -4,4 +4,5 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(blank=False, unique=True, max_length=36)
+    description = models.TextField(max_length=250, blank=True)
 
