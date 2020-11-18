336fa133d2949911839ee2d934eb7f42fc7901b0
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-18 11:47:35 +0000

    Added a new price field and used DecimalField type for this, also set options

--- demo/models.py
+++ demo/models.py
@@ -6,3 +6,5 @@ class Book(models.Model):
     title = models.CharField(blank=False, unique=True, max_length=36)
     description = models.TextField(max_length=250, blank=True)
 
+    price = models.DecimalField(default=0, max_digits=6, decimal_places=2)
+
