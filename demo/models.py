ecc33d4e1ef72d3fcfc9b6fc959bfb3e3b304d19
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-18 09:18:04 +0000

    Added Field Options for title CharField and tested in Django Administration

--- demo/models.py
+++ demo/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    title = models.CharField(max_length=36)
+    title = models.CharField(blank=False, unique=True)
+
