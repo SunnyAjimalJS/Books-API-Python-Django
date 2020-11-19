7da8a1fd3892afe003cef2745287778ae9618dcd
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-19 16:50:11 +0000

    Imported the Book model in to views and made a variable to get access to the Book objects and a variable to set the string format of the output

--- demo/views.py
+++ demo/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import Book
 # Create your views here.
 
 class Another(View):
+
+    books = Book.objects.all()
+
+    output = f"We have {len(books)} that many books in DB"
+
     def get(self, request):
         return HttpResponse('This is another function inside a class')
 
