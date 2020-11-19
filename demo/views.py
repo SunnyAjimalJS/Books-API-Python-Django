4381c7707bf285cf2f816cf0e9fc9f4d5c72cbb2
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-11-19 11:48:21 +0000

    Added a class function inside views, added to urls for Demo App and tested in browser

--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 # Create your views here.
+
+class Another(View):
+    def get(self, request):
+        return HttpResponse('This is another function inside a class')
+
 
 def first(request):
     return HttpResponse('First message from views!')
