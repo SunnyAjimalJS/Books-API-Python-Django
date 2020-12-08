57e223c2b077eb06478ccaf8dbfbc8b6c3da71fa
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-12-08 15:39:03 +0000

    Imported and registered the new Author model to the Django Admin and tested in browser - all working

--- demo/admin.py
+++ demo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, BookNumber, Character
+from .models import Book, BookNumber, Character, Author
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -10,4 +10,5 @@ class BookAdmin(admin.ModelAdmin):
 
 admin.site.register(BookNumber)
 admin.site.register(Character)
+admin.site.register(Author)
 
